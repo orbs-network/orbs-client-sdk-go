8f7d45b78123c7fe906df80e686dd42dfb987763
Tal Kol <talkol@gmail.com>
2018-11-29 21:21:11 +0200

    Version bump

--- gammacli/main.go
+++ gammacli/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-const GAMMA_CLI_VERSION = "0.5.0"
+const GAMMA_CLI_VERSION = "0.5.1"
 const CONFIG_FILENAME = "orbs-gamma-config.json"
 const TEST_KEYS_FILENAME = "orbs-test-keys.json"
 const LOCAL_ENV_ID = "local"

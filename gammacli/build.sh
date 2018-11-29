cb7bce96b9fdf8f14badaf7dfa19739f15a18be6
Tal Kol <talkol@gmail.com>
2018-11-29 22:15:45 +0200

    Make sure gamma-cli builds include version number for brew

--- gammacli/build.sh
+++ gammacli/build.sh
@@ -4,5 +4,5 @@ mkdir -p ./_bin
 go build -o ./_bin/gamma-cli
 
 # mac only for now
-tar -zcvf ./_bin/gammacli-mac.tar.gz ./_bin/gamma-cli
-openssl sha256 ./_bin/gammacli-mac.tar.gz
+tar -zcvf ./_bin/gammacli-mac-v1.2.3.tar.gz ./_bin/gamma-cli
+openssl sha256 ./_bin/gammacli-mac-v1.2.3.tar.gz

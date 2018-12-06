b8ddc485fa35beabb58235bfa905623b8920409d
Kirill Maksimov <kirill@orbs.com>
2018-12-06 17:07:18 +0200

    Fix gamma-cli tests

--- docker-test.sh
+++ docker-test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-python gammacli/test/generate_config.py > gammacli/test/orbs-gamma-config.json
+export GAMMA_ENDPOINT=$(python gammacli/test/generate_config.py endpoint)
+python gammacli/test/generate_config.py json > gammacli/test/orbs-gamma-config.json
+
 docker-compose -f ./docker-compose.test.yml up --abort-on-container-exit --exit-code-from sdk-e2e
--- gammacli/test/generate_config.py
+++ gammacli/test/generate_config.py
@@ -1,19 +1,24 @@
 import json
-import os
+import sys
 import socket
+
+def get_endpoint(ip):
+    return "http://" + ip + ":8080"
 
 def get_config(ip):
     return {
         "Environments": {
             "docker": {
                 "VirtualChain": 42,
-                "Endpoints": [
-                    "http://" + ip + ":8080"
-                ]
+                "Endpoints": [get_endpoint(ip)]
             },
         }
     }
 
 if __name__ == "__main__":
     ip = socket.gethostbyname(socket.gethostname())
-    print json.dumps(get_config(ip))
+
+    if sys.argv[1] == "endpoint":
+        print get_endpoint(ip)
+    elif sys.argv[1] == "json":
+        print json.dumps(get_config(ip))

a6dbc5dcf12eb72b67082e0f9eba2b3acf8955be
Tal Kol <talkol@gmail.com>
2019-01-08 16:19:20 +0200

    Upgraded test contract to contract sdk v1

--- gammacli/test/_counter/contract.go
+++ gammacli/test/_counter/contract.go
@@ -2,30 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/orbs-network/orbs-contract-sdk/go/sdk"
-	"github.com/orbs-network/orbs-contract-sdk/go/sdk/events"
-	"github.com/orbs-network/orbs-contract-sdk/go/sdk/state"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/events"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/state"
 )
 
 var PUBLIC = sdk.Export(add, get, start)
 var SYSTEM = sdk.Export(_init)
 var EVENTS = sdk.Export(Log)
 
+var COUNTER_KEY = []byte("count")
+
 func Log(msg string) {}
 
 func _init() {
-	state.WriteUint64ByKey("count", 0)
+	state.WriteUint64(COUNTER_KEY, 0)
 }
 
 func add(amount uint64) {
-	count := state.ReadUint64ByKey("count")
+	count := state.ReadUint64(COUNTER_KEY)
 	events.EmitEvent(Log, fmt.Sprintf("previous count is %d", count))
 	count += amount
-	state.WriteUint64ByKey("count", count)
+	state.WriteUint64(COUNTER_KEY, count)
 }
 
 func get() uint64 {
-	return state.ReadUint64ByKey("count")
+	return state.ReadUint64(COUNTER_KEY)
 }
 
 func start() uint64 {

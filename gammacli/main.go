e2399089a865ed7eab91680781d68a90058e0adf
Tal Kol <talkol@gmail.com>
2019-01-09 18:24:03 +0200

    Release 0.6.0

--- gammacli/main.go
+++ gammacli/main.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-const GAMMA_CLI_VERSION = "0.5.6"
+const GAMMA_CLI_VERSION = "0.6.0"
 const CONFIG_FILENAME = "orbs-gamma-config.json"
 const TEST_KEYS_FILENAME = "orbs-test-keys.json"
 const LOCAL_ENV_ID = "local"

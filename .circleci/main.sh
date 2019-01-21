35aa0bab2bb378b1ac0d60d1887df84ed816a411
itamar Arjuan <itamar.arjuan@gmail.com>
2019-01-21 18:23:19 +0200

    installing go

--- .circleci/main.sh
+++ .circleci/main.sh
@@ -5,6 +5,8 @@
 cd /tmp
 wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
 sudo tar -xvf go1.11.linux-amd64.tar.gz
+# Uninstall older version of Go
+sudo rm -rf /usr/local/go
 sudo mv go /usr/local
 
 export GOROOT=/usr/local/go

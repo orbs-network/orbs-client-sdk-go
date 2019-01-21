923efa9aae630e1147568bfcdcf0822dae3b3e75
itamar Arjuan <itamar.arjuan@gmail.com>
2019-01-21 18:20:48 +0200

    installing newer version of Go

---
+++ .circleci/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+
+# First let's install Go 1.11
+
+cd /tmp
+wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
+sudo tar -xvf go1.11.linux-amd64.tar.gz
+sudo mv go /usr/local
+
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+go version

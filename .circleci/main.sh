5e5902acab07f2da445635a518256e2716b4cf35
itamar Arjuan <itamar.arjuan@gmail.com>
2019-07-25 16:20:44 +0300

    ci mods

--- .circleci/main.sh
+++ .circleci/main.sh
@@ -8,7 +8,7 @@ echo "Installing Go 1.12..."
 cd /tmp
 
 curl -O https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
-sudo tar -xvf go${GO_VERSION}.linux-amd64.tar.gz
+sudo tar -xf go${GO_VERSION}.linux-amd64.tar.gz
 
 # Uninstall older version of Go
 sudo rm -rf /usr/local/go
